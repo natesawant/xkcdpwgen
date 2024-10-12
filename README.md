e05afaabefefb02194ec1bc3ae6409bb821e5078
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2024-10-12 05:09:34 +0000

    Update project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # xkcdpwgen
-A method of password generation using the method by XKCD
+> Generate a secure, memorable password using the XKCD method.
+
+Inspired from [this xkcd comic.](https://xkcd.com/936/)
+
+## Usage
+
+```bash
+python xkcdpwgen.py [args]
+```
+
+### Arguments
+
+| Short Hand | Long Hand | Description                                   |
+|------------|-----------|-----------------------------------------------|
+| -w         | --words   | include w words in the password               |
+| -c         | --caps    | capitalize the first letter of c random words |
+| -n         | --numbers | insert n random numbers in the password       |
+| -s         | --symbols | insert s random symbols in the password       |
--- xkcdpwgen.py
+++ xkcdpwgen.py
@@ -2,48 +2,38 @@ import argparse
 import random
 import requests
 
-word_list_url = "http://www.mieliestronk.com/corncob_lowercase.txt"
+word_list_url = "https://raw.githubusercontent.com/hugsy/stuff/refs/heads/main/random-word/english-nouns.txt"
 word_list = requests.get(word_list_url).text.split()
 
-desc = "Generate a secure, memorable password using the XKCD method. Written by Nate Sawant for CY2550."
+symbols_list = "~!@#$%^&*.:;"
 
+desc = "Generate a secure, memorable password using the XKCD method. Written by Nate Sawant for CY2550."
 parser = argparse.ArgumentParser(description=desc)
 
 parser.add_argument(
-    "-w", "--words", help="include WORDS words in the password\n(default=4)")
+    "-w", "--words", help="include WORDS words in the password", type=int, default=4)
 
 parser.add_argument(
-    "-c", "--caps", help="capitalize the first letter of CAPS random words\n(default=0)")
+    "-c", "--caps", help="capitalize the first letter of CAPS random words", type=int, default=0)
 
 parser.add_argument(
-    "-n", "--numbers", help="insert NUMBERS random numbers in the password\n(default=0)")
+    "-n", "--numbers", help="insert NUMBERS random numbers in the password", type=int, default=0)
 
 parser.add_argument(
-    "-s", "--symbols", help="insert SYMBOLS random symbols in the password\n(default=0)")
+    "-s", "--symbols", help="insert SYMBOLS random symbols in the password", type=int, default=0)
 
 args = parser.parse_args()
 
-words, caps, numbers, symbols = 4, 0, 0, 0
-
-try:
-    if (args.words):
-        words = int(args.words)
-    if (args.caps):
-        caps = int(args.caps)
-    if (args.numbers):
-        numbers = int(args.numbers)
-    if (args.symbols):
-        symbols = int(args.symbols)
-except:
-    print("Arguments must be numbers")
-    exit()
+words = args.words
+caps = args.caps
+numbers = args.numbers
+symbols = args.symbols
 
 if (caps > words):
     print("Caps must be less than or equal to words")
     exit()
 
 parts = []
-symbols_list = "~!@#$%^&*.:;"
 
 for i in range(words):
     parts.append(word_list[random.randrange(len(word_list))])
@@ -57,4 +47,4 @@ for i in range(symbols):
 
 random.shuffle(parts)
 
-print("".join(map(str, parts)))
+print("".join(str(p) for p in parts))
